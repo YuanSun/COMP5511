b0ccc51b52dd730ec0c82cf3f0ad4ec80f80cc7e
SunYuan <reggie.sunyuan@me.com>
2017-10-08 20:43:45 -0400

    Question 6 finished

--- Assignment1/src/assignment2/main.java
+++ Assignment1/src/assignment2/main.java
@@ -1,5 +1,6 @@
 package assignment2;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -7,7 +8,8 @@ import java.util.Stack;
 
 public class main {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+    
     LinkedList<Integer> linkedList = new LinkedList<>();
 
     // Question 4
@@ -39,7 +41,7 @@ public class main {
       singleLinkedList.insert(randomArray.remove(0));
     }
     
-    System.out.println("Sorted single linked list is :");
+    System.out.println("\nSorted single linked list is :");
     singleLinkedList.displayList();
     
     Stack<SingleLink<Integer>> stack = new Stack<>();
@@ -51,8 +53,36 @@ public class main {
       singleLinkedList.insert(stack.pop().data);
     }
     
-    System.out.println("Reversed single linked list is :");
+    System.out.println("\nReversed single linked list is :");
     singleLinkedList.displayList();
+  
+    
+    // Question 6
+    Q6 q6 = new Q6(); // accept data
+    
+    // get two copies
+    String[] str1 = new String[q6.data.size()]; 
+    q6.data.toArray(str1);
+    
+    String[] str2 = new String[q6.data.size()];
+    q6.data.toArray(str2);
+    
+    // use selection sort on str1, quick sort on str2
+    long startTime1 = System.nanoTime();
+    Q6.selectionSort(str1);
+    long sortTime1 = System.nanoTime() - startTime1;
+    
+    long startTime2 = System.nanoTime();
+    Q6.quickSort(str2);
+    long sortTime2 = System.nanoTime() - startTime2;
+    
+    // check sort results
+//    for (int i = 0; i < str1.length; i++) {
+//      System.out.println(str1[i] + " - " + str2[i]);
+//    }
+    
+    System.out.println("\nQuestion 6: \nSort time for selection sort is " + (double)sortTime1/1000000 + " mili seconds.\n"
+        + "Sort time for quick sort is " + (double)sortTime2/1000000 + " mili seconds.");
   }
 
 }
