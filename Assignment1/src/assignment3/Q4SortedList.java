0932a4d577dfae24d80592c0905a48e77cb1f0aa
Yuan Sun <Yuan.Sun@nakisa.com>
2017-10-26 14:08:46 -0400

    Ass3: Insertion sort implemented on Map

--- Assignment1/src/assignment3/Q4SortedList.java
+++ Assignment1/src/assignment3/Q4SortedList.java
@@ -1,7 +1,12 @@
 package assignment3;
 
 import java.io.FileNotFoundException;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class Q4SortedList implements Q4 {
@@ -9,6 +14,34 @@ public class Q4SortedList implements Q4 {
 
   public Q4SortedList() throws FileNotFoundException {
     studentGAP = this.getGPAData();
+  }
+
+  // * Use insertion algorithm to sort the map by value.
+  // * Insertion algorithm is implemented on two steps: step 2 and step 3
+  // * step 2: compare the entry
+  // * step 3: insert the entry to a new map based on the comparison
+  private static Map<String, Double> sortByValue(Map<String, Double> unsortMap) {
+
+    // 1. Convert Map to List of Map
+    List<Map.Entry<String, Double>> list =
+        new LinkedList<Map.Entry<String, Double>>(unsortMap.entrySet());
+
+    // 2. Sort list with Collections.sort(), provide a custom Comparator
+    // Try switch the o1 o2 position for a different order
+    Collections.sort(list, new Comparator<Map.Entry<String, Double>>() {
+      public int compare(Map.Entry<String, Double> o1,
+          Map.Entry<String, Double> o2) {
+        return (o1.getValue()).compareTo(o2.getValue());
+      }
+    });
+
+    // 3. Loop the sorted list and put it into a new insertion order Map LinkedHashMap
+    Map<String, Double> sortedMap = new LinkedHashMap<String, Double>();
+    for (Map.Entry<String, Double> entry : list) {
+      sortedMap.put(entry.getKey(), entry.getValue());
+    }
+
+    return sortedMap;
   }
 
   @Override
