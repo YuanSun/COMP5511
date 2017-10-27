a481cb7500d210162ba462fc798b50e34e4d0e89
Yuan Sun <Yuan.Sun@nakisa.com>
2017-10-27 15:16:52 -0400

    test on heap

--- Assignment1/src/assignment3/Heap.java
+++ Assignment1/src/assignment3/Heap.java
@@ -2,6 +2,7 @@ package assignment3;
 
 import java.util.AbstractMap.SimpleEntry;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 
 /*
@@ -39,32 +40,85 @@ public class Heap<K, V> extends ArrayList<Map.Entry<String, Double>> {
     return this.get(indexOf(T) * 2 + 2);
   }
 
+  // ------------------------------------------------------------------------------
   // Here the node at index i and its two direct children violates the heap property
-  public void trickleDown(int i) {
+  private void trickleDown(int index) {
     Map.Entry<String, Double> smallest;
-    Map.Entry<String, Double> top = this.get(i);
+    Map.Entry<String, Double> top = this.get(index);
     int currentSize = this.size();
 
-    while (i < currentSize / 2) {
-      Map.Entry<String, Double> left = LEFT(this.get(i));
-      Map.Entry<String, Double> right = RIGHT(this.get(i));
+    while (index < currentSize / 2) { // while node has at leat one child
+      Map.Entry<String, Double> left = LEFT(this.get(index));
+      Map.Entry<String, Double> right = RIGHT(this.get(index));
 
       // find smaller child
       // if right child exists
-      if (left.getValue().compareTo(right.getValue()) > 0) {
+      if (right != null && left.getValue().compareTo(right.getValue()) > 0) {
         smallest = right;
+      } else {
+        smallest = left;
       }
+
+      // top <= smallest ?
+      if (top.getValue().compareTo(smallest.getValue()) <= 0) {
+        break;
+      }
+
+      // shift child up
+      this.set(index, smallest);
+      index = this.indexOf(smallest);
+    } // end while
+
+    this.set(index, top);
+  }
+
+  // ------------------------------------------------------------------
+  // trickleUp-- make the tree satisfy MinHeap condition
+  private void trickleUp(int index) {
+    Map.Entry<String, Double> bottom = this.get(index);
+
+    // if bottom < parent, move up
+    while (PARENT(bottom).getValue().compareTo(bottom.getValue()) > 0) {
+      Collections.swap(this, this.indexOf(bottom), this.indexOf(PARENT(bottom)));
     }
 
   }
 
+  // -----------------------------------------------------------------
+  public boolean change(int index, Map.Entry<String, Double> newEntry) {
+    if (index < 0 || index >= this.size()) {
+      return false;
+    }
+
+    Map.Entry<String, Double> oldEntry = this.get(index);
+    this.add(index, newEntry);
+
+    if (oldEntry.getValue().compareTo(newEntry.getValue()) < 0) {
+      trickleUp(index);
+    } else {
+      trickleDown(index);
+    }
+
+    return true;
+
+
+  }
+
+  // -------------------------------------------------------------------
   public boolean add(String key, Double value) {
     Map.Entry<String, Double> e = new SimpleEntry<String, Double>(key, value);
 
     // add the node to the heap, then trickleDown
-    return this.add(e);
+    this.add(e);
+    trickleUp(this.indexOf(e));
+    return true;
   }
 
-
+  // -------------------------------------------------------------------------
+  public void displayHeap() {
+    this.forEach(entry -> {
+      System.out.println(this.indexOf(entry) + ": " + entry.getKey() + ": " + entry.getValue());
+    });
+  }
 
 }
--- Assignment1/src/assignment3/main.java
+++ Assignment1/src/assignment3/main.java
@@ -13,6 +13,13 @@ public class main {
 
     Heap<String, Double> heap = new Heap<>();
     heap.add("John", 3.23);
+    heap.add("Helan", 4.35);
+    heap.add("Joen", 4.33);
+    heap.add("Jack", 1.02);
+    heap.add("Shred", 2.34);
+    heap.add("Gray", 3.76);
+    heap.add("Bob", 4.00);
+    heap.displayHeap();
   }
 
 }
