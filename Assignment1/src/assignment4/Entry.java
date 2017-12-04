b2348749bda209d9e24ad986c9c8ad388b1f7ce2
SunYuan <reggie.sunyuan@me.com>
2017-12-04 15:02:03 -0500

    Initial submit of assignment 4

---
+++ Assignment1/src/assignment4/Entry.java
@@ -0,0 +1,92 @@
+package assignment4;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Entry implements Record{
+  private String name;
+  private String email;
+  private String school;
+  private String country;
+  
+  public String getName() {
+    return this.name;
+  }
+  
+  public String getEmail() {
+    return this.email;
+  }
+  
+  public String getSchool() {
+    return this.school;
+  }
+  
+  public String getCountry() {
+    return this.country;
+  }
+
+  @Override
+  public Record getRecord() {
+    return this;
+  }
+
+  @Override
+  public void displayRecord() {
+    System.out.printf("%60s | %120s | %60 | %30s", 
+        this.name, this.email, this.school, this.country);
+    
+  }
+  
+  private Entry(EntryBuilder builder) {
+    this.name = builder.name;
+    this.email = builder.email;
+    this.school = builder.school;
+    this.country = builder.country;
+  }
+  
+  public static class EntryBuilder {
+    // required part : name
+    private String name;
+    
+    // Optional parts
+    private String email;
+    private String school;
+    private String country;
+    
+    public EntryBuilder (String name) {
+      this.name = name;
+    }
+    
+    public EntryBuilder setEmail(String email) {
+      if(isValid(email)) {
+        this.email = email;
+      }
+      return this;
+    }
+
+    private boolean isValid(String email2Validate) {
+      Pattern p = Pattern.compile("\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b");
+      Matcher m = p.matcher(email2Validate);
+      
+      if (m.find()) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+    
+    public EntryBuilder setSchool(String school) {
+      this.school = school;
+      return this;
+    }
+    
+    public EntryBuilder setCountry(String country) {
+      this.country = country;
+      return this;
+    }
+    
+    public Entry build() {
+      return new Entry(this);
+    }
+  }
+}
---
+++ Assignment1/src/assignment4/Record.java
@@ -0,0 +1,8 @@
+package assignment4;
+
+public interface Record {
+
+  public Record getRecord();
+  
+  public void displayRecord();
+}
