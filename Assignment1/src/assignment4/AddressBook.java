259f54531c20db2918e21712021391e1912b3c25
SunYuan <reggie.sunyuan@me.com>
2017-12-04 16:02:19 -0500

    Address book

--- Assignment1/src/assignment4/AddressBook.java
+++ Assignment1/src/assignment4/AddressBook.java
@@ -1,5 +1,13 @@
 package assignment4;
 
+import java.util.ArrayList;
+
 public class AddressBook {
+  final String inputFile = "\\src\\assignment4\\ds17s-Asg4-data.txt";
   
+  public ArrayList<Entry> addressBook = new ArrayList<>();
+  
+  public AddressBook() {
+    
+  }
 }
