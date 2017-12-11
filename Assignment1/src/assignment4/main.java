b7aec341b3c9c47490914fa097e14b61c0951296
SunYuan <reggie.sunyuan@me.com>
2017-12-10 23:29:04 -0500

    Final change for the assignment

--- Assignment1/src/assignment4/main.java
+++ Assignment1/src/assignment4/main.java
@@ -6,15 +6,31 @@ import java.util.ArrayList;
 public class main {
 
   public static void main(String[] args) throws IOException {
+    // Question 1
+    System.out.println("Question 1:");
     AddressBook addressBook = new AddressBook();
+    String searchKey = "anca";
+    System.out.println("Searchh with \"" + searchKey + "\"");
+    ArrayList<Entry> queryResult = addressBook.searchWithPattern(searchKey);
+    
+    searchKey = "McGill";
+    System.out.println("\nSearch \"" + searchKey + "\"");
+    addressBook.searchWithPattern(searchKey);
 
-
-    ArrayList<Entry> queryResult = addressBook.searchWithPattern("anca");
-
-
+    //Question 2
+    searchKey = "CA"; // search Country
+    System.out.println("\nQuestion2:");
+    System.out.println("Search country");
     addressBook.createIndex(IndexType.COUNTRY);
     addressBook.searchWithIndex(IndexType.COUNTRY, "CA");
+    
+    searchKey = "Technology";
+    System.out.println("\nSearch organization");
+    addressBook.createIndex(IndexType.ORGANIZATION);
+    addressBook.searchWithIndex(IndexType.ORGANIZATION, searchKey);
+    
 
+    System.out.println("\nQuestion 3:");
     BPlusTreeSearch treeSearch = new BPlusTreeSearch();
     treeSearch.displayResult();
 
