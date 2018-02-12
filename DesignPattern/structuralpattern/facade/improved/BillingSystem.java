39b6f37261485a29280a7445d5b831f0e014df2a
SunYuan <reggie.sunyuan@me.com>
2018-02-12 06:37:44 -0500

    structural Design pattern

---
+++ DesignPattern/structuralpattern/facade/improved/BillingSystem.java
@@ -0,0 +1,12 @@
+package facade.improved;
+
+public class BillingSystem {
+
+	public Bill createBill(Integer amount){
+		
+		// Let's assume some advanced logic happens here;
+		return new Bill(amount);
+		
+	}
+	
+}
