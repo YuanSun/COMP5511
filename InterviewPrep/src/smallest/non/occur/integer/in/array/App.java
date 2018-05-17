7b42d996e713f885096512c8dde98f870a8bf6b5
SunYuan <reggie.sunyuan@me.com>
2018-05-17 01:17:56 -0400

    Add a main

---
+++ InterviewPrep/src/smallest/non/occur/integer/in/array/App.java
@@ -0,0 +1,11 @@
+package smallest.non.occur.integer.in.array;
+
+public class App {
+
+  public static void main(String[] args) {
+    int[] A = {1, 3, 6, 4, 1, 2};
+    
+    System.out.println("Smallest non occur integer is " + Solution.solution(A));
+  }
+
+}
