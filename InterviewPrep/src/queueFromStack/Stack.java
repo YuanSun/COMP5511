36ce718e6d881f0076dfb509b6707bba393697dd
SunYuan <reggie.sunyuan@me.com>
2018-05-17 20:03:03 -0400

    rename QueueFromStack

--- InterviewPrep/src/queueFromStack/Stack.java
+++ InterviewPrep/src/queueFromStack/Stack.java
@@ -1,4 +1,4 @@
-package qFromS;
+package queueFromStack;
 
 import java.util.ArrayList;
 import java.util.Collection;
--- InterviewPrep/src/queueFromStack/StackQueue.java
+++ InterviewPrep/src/queueFromStack/StackQueue.java
@@ -1,4 +1,4 @@
-package qFromS;
+package queueFromStack;
 
 import java.util.Collection;
 
